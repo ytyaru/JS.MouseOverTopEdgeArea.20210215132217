75643229a6e5562224d3786d70c05cd09153c804
ytyaru <yttry0@gmail.com>
2021-02-15 17:41:42 +0900

    ES Module

---
+++ docs/4/main.js
@@ -0,0 +1,6 @@
+import Loader from './js/common/Loader.js';
+import MouseOverTopEdgeArea from './js/topedge/MouseOverTopEdgeArea.js';
+window.addEventListener('load', async(event) => {
+    MouseOverTopEdgeArea.Template = await Loader.load('./css-template/top-edge.css');
+    MouseOverTopEdgeArea.parse();
+});
