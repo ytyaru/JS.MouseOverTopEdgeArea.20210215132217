68214eb21c3139aeb67458589f94780cbaf8e995
ytyaru <yttry0@gmail.com>
2021-02-15 17:42:36 +0900

    clean

--- docs/3/js/topedge/MouseOverTopEdgeAreaStyle.js
+++ docs/3/js/topedge/MouseOverTopEdgeAreaStyle.js
@@ -39,38 +39,4 @@ class MouseOverTopEdgeAreaStyle {
         }
         return css;
     }
-        /*
-    replace(colors) {
-        const style = document.querySelector(`#${this.#id}`) || this.#createStyle();
-        console.log('style:', style);
-        style.textContent = this.#replaceTemplate(colors);
-        return this.#id;
-    }
-    #replaceTemplate(colors) {
-        const css = this.#template || 
-        return `
-            {{query}} {
-                position: fixed;
-                right: 0;
-                display: none;
-            }
-            {{query}}:hover .dropdown{
-                display: inline;
-            }
-        `;
-        this.#regexps = [];
-        this.#regexps.push({regexp: new RegExp('{{[\s]*query[\s]*}}', 'g'), replace: this.#query});
-        for (let key of ['back', 'fore', 'selectedBack', 'selectedFore', 'shadow']) {
-            this.#regexps.push({regexp: new RegExp(`{{[\s]*${key}[\s]*}}`, 'g'), replace: colors[`${key}`]});
-        }
-        console.log(this.#regexps);
-        let css = this.#template;
-        for (let regexp of this.#regexps) {
-            css = css.replace(regexp.regexp, regexp.replace);
-        }
-        console.log(colors);
-        console.log(css);
-        return css;
-    }
-        */
 }
